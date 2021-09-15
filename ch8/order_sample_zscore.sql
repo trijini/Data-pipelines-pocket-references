6f7432748283cc2320931c410251db33eca53720
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-09-16 01:46:06 +0900

    correct typos

--- ch8/order_sample_zscore.sql
+++ ch8/order_sample_zscore.sql
@@ -2,10 +2,10 @@ WITH order_count_zscore AS (
     SELECT
         order_date,
         order_count,
-        (order_count - avg(order_count) over()
-        / (stddev(order_count))) as z_score
-    FROM orders_by py
+        (order_count - avg(order_count) over())
+        / (stddev(order_count) over()) as z_score
+    FROM orders_by_day
 )
 SELECT ABS(z_score) AS twosided_score
 FROM order_count_zscore
-WHERE order_date = (CAST('2020-10-05' AS DATE, - inverval '1 day'))
+WHERE order_date = CAST('2020-10-05' AS DATE) - interval '1 day';
